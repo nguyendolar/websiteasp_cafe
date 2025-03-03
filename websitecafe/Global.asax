1c9af0af93d4eea3f869f7b403cf94f52667847d
Your Name <you@example.com>
2025-03-03 20:31:42 +0700

    first commit

---
+++ websitecafe/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="websitecafe.MvcApplication" Language="C#" %>
---
+++ websitecafe/Models/DBWebcafeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace websitecafe.Models
+{
+	public class DBWebcafeContext : DbContext
+    {
+        public DBWebcafeContext() : base("DBConnectionString")
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
