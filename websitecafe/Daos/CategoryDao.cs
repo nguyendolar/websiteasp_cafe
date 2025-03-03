91fda9303b6d8b34549b0bdce25902fddaebefd7
Your Name <you@example.com>
2025-03-03 21:44:26 +0700

    'csdl'

---
+++ websitecafe/Daos/CategoryDao.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using websitecafe.Models;
+
+namespace websitecafe.DAO
+{
+    public class CategoryDao
+    {
+        private readonly DBWebcafeContext _context;
+
+        public CategoryDao(DBWebcafeContext context)
+        {
+            _context = context;
+        }
+
+        public List<Category> GetAllCategories()
+        {
+            return _context.Categories.ToList();
+        }
+
+        public Category GetCategoryById(int id)
+        {
+            return _context.Categories.FirstOrDefault(c => c.Id == id);
+        }
+
+        public void AddCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            var existingCategory = _context.Categories.Find(category.Id);
+            if (existingCategory != null)
+            {
+                existingCategory.Name = category.Name;
+                _context.SaveChanges();
+            }
+        }
+
+        public void DeleteCategory(int id)
+        {
+            var category = _context.Categories.Find(id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
